a540abacdde6daf635e288e28015bb316582da81
Kaled Goatache <98194422+KaledGoatache@users.noreply.github.com>
2025-02-13 02:24:12 +0100

    Add files via upload

---
+++ tradingstrategyMA.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#import FB's stock data, add two columns - MA10 and MA50
+#use dropna to remove any "Not a Number" data
+ms = pd.DataFrame.from_csv('../data/microsoft.csv')
+ms['MA10'] = ms['Close'].rolling(10).mean()
+ms['MA50'] = ms['Close'].rolling(50).mean()
+ms = ms.dropna()
+ms.head()
+
+#Add a new column "Shares", if MA10>MA50, denote as 1 (long one share of stock), otherwise, denote as 0 (do nothing)
+
+ms['Shares'] = [1 if ms.loc[ei, 'MA10']>ms.loc[ei, 'MA50'] else 0 for ei in ms.index]
+
+#Add a new column "Profit" using List Comprehension, for any rows in fb, if Shares=1, the profit is calculated as the close price of
+#tomorrow - the close price of today. Otherwise the profit is 0.
+
+#Plot a graph to show the Profit/Loss
+
+ms['Close1'] = ms['Close'].shift(-1)
+ms['Profit'] = [ms.loc[ei, 'Close1'] - ms.loc[ei, 'Close'] if ms.loc[ei, 'Shares']==1 else 0 for ei in ms.index]
+ms['Profit'].plot()
+plt.axhline(y=0, color='red')
+
+#Use .cumsum() to calculate the accumulated wealth over the period
+
+ms['wealth'] = ms['Profit'].cumsum()
+ms.tail()
+
+#plot the wealth to show the growth of profit over the period
+
+ms['wealth'].plot()
+plt.title('Total money you win is {}'.format(ms.loc[ms.index[-2], 'wealth']))
